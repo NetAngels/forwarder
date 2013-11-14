1f33a44a609feef30ccb0a28cdc6a5b94b305b35
Michael Elovskikh <wronglink@gmail.com>
2013-11-14 17:46:26 +0600

    Добавил возможность TLS-шифрования подключения
    refs #3581

--- forwarder/__main__.py
+++ forwarder/__main__.py
@@ -3,7 +3,8 @@
 import logging
 import sys
 
-from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import ioloop
+from tornado.options import options
 
 from forwarder import ForwardServer
 
@@ -12,10 +13,24 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(
 
 
 def main():
-    server = ForwardServer(sys.argv[1])
+    options.define('certfile', help="Path to SSL certificate to enable TSL")
+    options.define('keyfile', help="Path to SSL key to enable TSL")
+    unparsed = options.parse_command_line()
+    if len(unparsed) == 1:
+        confpath = options.parse_command_line()[0]
+    else:
+        raise ValueError("Configuration file is not specified")
+    if options.certfile and options.keyfile:
+        ssl_options = {
+            "certfile": options.certfile,
+            "keyfile": options.keyfile,
+        }
+    else:
+        ssl_options = None
+    server = ForwardServer(confpath, ssl_options=ssl_options)
     server.bind_from_conf()
-    PeriodicCallback(server.bind_from_conf, 500).start()
-    IOLoop.instance().start()
+    ioloop.PeriodicCallback(server.bind_from_conf, 500).start()
+    ioloop.IOLoop.instance().start()
 
 
 if __name__ == '__main__':
